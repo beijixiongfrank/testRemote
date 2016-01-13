34b00b7c4b4a33a4e90f59b98f746156dcdd7127
wangzw <wangzw@mchz.com.cn>
2016-01-13 14:35:32 +0800

    test branch

--- TestGit/src/com/wzw/test/TestHelloGit.java
+++ TestGit/src/com/wzw/test/TestHelloGit.java
@@ -5,6 +5,8 @@ public class TestHelloGit {
 	public TestHelloGit() {
 		// TODO Auto-generated constructor stub
 		System.out.println("Hello  Git!");
+		System.out.println("It has been modifyed!");
+		System.out.println("This is a branch!");
 	}
 
 }
