7e2f1bfd4000218c0fe442aad5c160f23fa9d41c
wangzw <wangzw@mchz.com.cn>
2016-01-13 14:59:24 +0800

    Revert "handle conflict"
    This reverts commit cbc657d0312af12f8a969667d3cb155c6ebe1720.

---
+++ TestGit/src/com/wzw/test/TestHelloGit.java
@@ -0,0 +1,11 @@
+package com.wzw.test;
+
+public class TestHelloGit {
+
+	public TestHelloGit() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Hello  Git!");
+		System.out.println("This is branch2!");
+	}
+
+}
