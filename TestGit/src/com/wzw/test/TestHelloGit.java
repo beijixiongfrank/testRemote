2448be3ce51290709b4f2a73e94cca58dca2e15c
wangzw <wangzw@mchz.com.cn>
2016-01-13 14:48:05 +0800

    Merge remote-tracking branch 'refs/remotes/origin/branch2'

--- TestGit/src/com/wzw/test/TestHelloGit.java
+++ TestGit/src/com/wzw/test/TestHelloGit.java
@@ -5,6 +5,7 @@ public class TestHelloGit {
 	public TestHelloGit() {
 		// TODO Auto-generated constructor stub
 		System.out.println("Hello  Git!");
+		System.out.println("This is branch2!");
 	}
 
 }
---
+++ TestGit/src/com/wzw/test/TestMerge.java
@@ -0,0 +1,10 @@
+package com.wzw.test;
+
+public class TestMerge {
+
+	public TestMerge() {
+		// TODO Auto-generated constructor stub
+		System.out.println("This is a new method!");
+	}
+
+}
