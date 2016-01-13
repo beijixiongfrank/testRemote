a0e56ddfaf9a82bf11ad509e72f9455184b36583
wangzw <wangzw@mchz.com.cn>
2016-01-13 16:24:39 +0800

    new stuff added

---
+++ TestGit/src/com/wzw/test/TestPullRequest.java
@@ -0,0 +1,10 @@
+package com.wzw.test;
+
+public class TestPullRequest {
+
+	public TestPullRequest() {
+		// TODO Auto-generated constructor stub
+		System.out.println("This is some new stuff!");
+	}
+
+}
